3a9c650be87055784cb8f4c22a4d49769ce4fd24
zhouhw0306 <2466864587@qq.com>
2023-03-24 23:00:28 +0800

    2023/3/24

--- forum-client/src/App.vue
+++ forum-client/src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <div id="app">
-
     <router-view/>
+    <go-top></go-top>
   </div>
 </template>
 
+<script>
+import GoTop from '@/components/gotop/GoTop'
+export default {
+  name: 'App',
+  components: { GoTop }
+}
+</script>
 
 <style>
 
